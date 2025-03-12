fdba222b3d312ad97157d2b2e021a2c020011e0a
aliska88 <aliska_88k@mail.ru>
2025-03-13 02:54:36 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,196 +1,112 @@
 #include <stdio.h>
-#include <random>
-#include <time.h>
+#include <math.h>
+#include <malloc.h>
 
-bool valid(const double* array, int lenght) {
-	for (int i = 0; i < lenght - 1; i++) {
-		if (array[i] > array[i + 1])
-			return 0;
-	}
-	return 1;
+float sin_(float x, int n, float x_n) {
+    if (n == 0)
+        return x;
+    return -x_n * (x * x) / (4 * n * n + 2 * n);
+}
+float cos_(float x, int n, float x_n) {
+    if (n == 0)
+        return 1.0;
+    return -x_n * (x * x) / (4 * n * n - 2 * n);
+}
+float exp_(float x, int n, float x_n) {
+    if (n == 0)
+        return 1.0;
+    return x_n * (x / n);
+}
+float ln_(float x, int n, float x_n) {
+    if (n == 0)
+        return x;
+    return -x_n * (n * x) / (n + 1);
 }
 
-int brush_sort(double* array, int lenght) {
-	int operationCount = 0;
-	int step = lenght / 2;
-	for (; step > 1; step /= 2) {
-		for (int i = 0; i < step; i++) {
-			for (int j = i; j + step < lenght; j += step) {
-				if (array[j] > array[j + step]) {
-					double t = array[j];
-					array[j] = array[j + step];
-					array[j + step] = t;
-					++operationCount;
-				}
-			}
-		}
-	}
-	if (!valid(array, lenght)) {
-		for (int i = 0; i < lenght - 1; i++) {
-			for (int j = 0; j < lenght - i - 1; j++) {
-				if (array[j] > array[j + 1]) {
-					double t = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = t;
-					operationCount++;
-				}
-			}
-		}
-	}
-  return operationCount;
+float forvardSum(float x, int n, float(*fptr)(float, int, float)) {
+    float sum = 0, prev = 0;
+    for (int i = 0; i <= n; i++) {
+        prev = fptr(x, i, prev);
+        sum += prev;
+    }
+    return sum;
+}
+float reverseSum(float x, int n, float(*fptr)(float, int, float)) {
+    float sum = 0, prev = 0;
+    float* t_arr = (float*)calloc(n + 1, sizeof(float));
+    for (int i = 0; i <= n; i++)
+        t_arr[i] = prev = fptr(x, i, prev);
+    for (int i = n; i != -1; i--)
+        sum += t_arr[i];
+    free(t_arr);
+    return sum;
+}
+float twinsSum(float x, int n, float(*fptr)(float, int, float)) {
+    float sum = 0, prev = 0;
+    float* t_arr = (float*)calloc(n + 1, sizeof(float));
+    for (int i = 0; i <= n; i++)
+        t_arr[i] = prev = fptr(x, i, prev);
+    for (int i = 0; i < n / 2 * 2; i += 2)
+        sum += t_arr[i] + t_arr[i + 1];
+    if (n % 2 != 0)
+        sum += t_arr[n];
+    return sum;
 }
 
-int selection_sort(double* array, int lenght) {
-	int operationCount = 0;
-	for (int i = 0; i < lenght - 1; i++) {
-		int min = i;
-		for (int j = i + 1; j < lenght; j++) {
-			if (array[j] < array[min]) {
-				min = j;
-				operationCount++;
-			}
-		}
-		double t = array[i];
-		array[i] = array[min];
-		array[min] = t;
-	}
-  return operationCount;
-}
+void main() {
+    printf("1) Sin(x) \n2) Cos(x) \n3) Exp(x) \n4) Ln(x + 1) \n");
+    int func_c = 0;
+    float(*fptr)(float, int, float) = NULL;
+    double(*eFptr)(double) = NULL;
+    scanf_s("%d", &func_c);
+    switch (func_c) {
+    case 1:
+        fptr = sin_;
+        eFptr = sin;
+        break;
+    case 2:
+        fptr = cos_;
+        eFptr = cos;
+        break;
+    case 3:
+        fptr = exp_;
+        eFptr = exp;
+        break;
+    case 4:
+        fptr = ln_;
+        eFptr = log1p;
+        break;
 
-void merge(double* array, int left, int mid, int right, int* operationCount) {
-	double* lArray = (double*)malloc(sizeof(double) * (mid - left + 1));
-	double* rArray = (double*)malloc(sizeof(double) * (right - mid));
-  for (int i = 0; i < mid - left + 1; i++)
-    lArray[i] = array[left + i];
-	for (int i = 0; i < right - mid; i++)
-		rArray[i] = array[mid + 1 + i];
-	int lIndex = 0, rIndex = 0, mIndex = left;
-	while (lIndex < mid - left + 1 && rIndex < right - mid) {
-		if (lArray[lIndex] < rArray[rIndex]) {
-      array[mIndex] = lArray[lIndex];
-			lIndex++;
-			(*operationCount)++;
-		}
-		else {
-			array[mIndex] = rArray[rIndex];
-			rIndex++;
-			(*operationCount)++;
-		}
-		mIndex++;
-	}
-  while (lIndex < mid - left + 1) {
-    array[mIndex] = lArray[lIndex];
-    lIndex++;
-    mIndex++;
-		(*operationCount)++;
-  }
-  while (rIndex < right - mid) {
-    array[mIndex] = rArray[rIndex];
-    rIndex++;
-    mIndex++;
-		(*operationCount)++;
-  }
-  free(lArray);
-	free(rArray);
-}
-void merge_sort(double* array, int left, int right, int* operationCount) {
-	if (left < right) {
-		int mid = left + (right - left) / 2;
-		merge_sort(array, left, mid, operationCount);
-		merge_sort(array, mid + 1, right, operationCount);
-		merge(array, left, mid, right, operationCount);
-	}
-}
+    default:
+        break;
+    }
 
-void count(double mas[], int counter[], int lenght, int offset) {
-	char* b = (char*)mas;
-	b = b + offset;
-	memset(counter, 0, 256 * sizeof(int));
-	for (int i = 0; i < lenght; i++) {
-		counter[(unsigned char)*b]++;
-		b += sizeof(double);
-	}
-	int s = 0, tmp;
-	for (int i = 0; i < 256; i++) {
-		tmp = counter[i];
-		counter[i] = s;
-		s += tmp;
-	}
-}
-int radix_sort(double* array, int lenght) {
-	int operationCount = 0;
-	int counter[256];
-	double* b = (double*)malloc(lenght * sizeof(double));
-	char* c = (char*)array;
-	for (int i = 0; i < sizeof(double); i++) {
-		count(array, counter, lenght, i);
-		for (int j = 0; j < lenght; j++) {
-			operationCount++;
-			b[counter[(unsigned char)*(c + i + j * sizeof(double))]++] = array[j];
-		}
-		for (int j = 0; j < lenght; j++) {
-			operationCount++;
-			array[j] = b[j];
-		}
-	}
-	int index;
-	for (index = 0; index < lenght; index++) {
-		if (b[index] < 0.0) {
-			operationCount++;
-			break;
-		}
-	}
-	for (int i = 0; i < lenght - index; i++) {
-		operationCount++;
-		array[i] = b[lenght - 1 - i];
-	}
+    printf("1) Forvard \n2) Reverce \n3) Twins \n");
+    int sum_c = 0;
+    scanf_s("%d", &sum_c);
 
-	for (int i = lenght - index; i < lenght; i++) {
-		operationCount++;
-		array[i] = b[i - lenght + index];
-	}
-	free(b);
-  return operationCount;
-}
+    printf("Enter x: \n");
+    float x = 0.0;
+    scanf_s("%f", &x);
 
-int main() {
-	srand(time(0));
-	int lenght, operation;
-	printf("Enter array size: ");
-	scanf_s("%d", &lenght);
-	clock_t start, end;
-	double* array = (double*)malloc(sizeof(double) * lenght);
-	
-	for (int i = 0; i < lenght; ++i) {
-		array[i] = (double)rand() / (double)(RAND_MAX / 100);
-	}
-  start = clock();
-	operation = brush_sort(array, lenght);
-	end = clock();
-  printf("\nThe brush sorting is done correctly: %d, operation count: %d, time: %f\n", valid(array, lenght), operation, (float)(end - start) / CLOCKS_PER_SEC);
+    printf("Enter max n: \n");
+    int n = 0;
+    scanf_s("%d", &n);
 
-	for (int i = 0; i < lenght; ++i) {
-		array[i] = (double)rand() / (double)(RAND_MAX / 100);
-	}
-	start = clock();
-	operation = selection_sort(array, lenght);
-	end = clock();
-	printf("\nThe selection sorting is done correctly: %d, operation count: %d, time: %f\n", valid(array, lenght), operation, (float)(end - start) / CLOCKS_PER_SEC);
+    for (int i = 1; i <= n; ++i) {
+        switch (sum_c) {
+        case 1:
+            printf("n = %d, err = %f \n", i, fabs(eFptr(x) - forvardSum(x, i, fptr)));
+            break;
+        case 2:
+            printf("n = %d, err = %f \n", i, fabs(eFptr(x) - reverseSum(x, i, fptr)));
+            break;
+        case 3:
+            printf("n = %d, err = %f \n", i, fabs(eFptr(x) - twinsSum(x, i, fptr)));
+            break;
 
-	for (int i = 0; i < lenght; ++i) {
-		array[i] = (double)rand() / (double)(RAND_MAX / 100);
-	}
-	start = clock();
-	operation = 0;
-	merge_sort(array, 0, lenght, &operation);
-	end = clock();
-	printf("\nThe merge sorting is done correctly: %d, operation count: %d, time: %f\n", valid(array, lenght), operation, (float)(end - start) / CLOCKS_PER_SEC);
-
-	for (int i = 0; i < lenght; ++i) {
-		array[i] = (double)rand() / (double)(RAND_MAX / 100);
-	}
-	start = clock();
-	operation = radix_sort(array, lenght);
-	end = clock();
-	printf("\nThe radix sorting is done correctly: %d, operation count: %d, time: %f\n", valid(array, lenght), operation, (float)(end - start) / CLOCKS_PER_SEC);
+        default:
+            break;
+        }
+    }
 }
